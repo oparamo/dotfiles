3610668000c29ccf7a5a0ca2f9fe61f41182d6ac
oscarparamo <oscarparamo@nectarom.com>
2015-02-18 12:59:10 -0600

    Fix install script paths.

--- main.sh
+++ main.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # this script copies over my config files to their proper locations
-# uncomment the lines for the config files you want to be used on your machine
+# comment out the lines for the config files you don't want to use
 
 ## Copy bash config files ##
 # copy over bash_profile
-#echo 'copying bash_profile'; cp bash/.bash_profile ~/;
+echo 'copying bash_profile'; cp bash/.bash_profile ~/;
 # copy over bashrc
-#echo 'copying bashrc'; cp bash/.bashrc ~/;
+echo 'copying bashrc'; cp bash/.bashrc ~/;
 # copy over inputrc
-#echo 'copying inputrc'; cp bash/.inputrc ~/;
+echo 'copying inputrc'; cp bash/.inputrc ~/;
 
 ## Copy git config files ##
 # copy over gitconfig
-#echo 'copying gitconfig'; cp bash/.gitconfig ~/;
+echo 'copying gitconfig'; cp git/.gitconfig ~/;
 # copy over gitignore_global
-#echo 'copying gitignore_global'; cp bash/.gitignore_global ~/;
+echo 'copying gitignore_global'; cp git/.gitignore_global ~/;
 # copy over git-completion
-#echo 'copying git-completion'; cp bash/.git-completion.bash ~/;
+echo 'copying git-completion'; cp git/git-completion.bash ~/;
